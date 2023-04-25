5f0aa82aa739c0e02723a397813214b50a7558d4
sahanagithub <114812803+sahanagithub@users.noreply.github.com>
2023-04-25 10:38:41 +0530

    Add files via upload

---
+++ 01_introduction/01_introduction/formatspecifier.c
@@ -0,0 +1,23 @@
+/*application: formatspecifier.c
+ * brief:printing formatspecifier of data type
+ * author:sahana.s
+ * title:project engineer
+ * date:23/03/2023*/
+#include<stdio.h>
+int main()
+{
+int a=123456;
+char c='A';
+float f=3.2;
+double d=987.123456;
+char str[]="RUGGEDBORD";
+
+printf("%d\n",a);
+printf("%c\n",c);
+printf("%f\n",f);
+printf("%lf\n",d);
+printf("%s\n",str);
+
+
+	return 0;
+}
---
+++ 01_introduction/01_introduction/hello.c
@@ -0,0 +1,13 @@
+/*Application:hello.c
+ * brief:this application is to print a greeting message on console.
+ * Author:sahana.s
+ * title:project engineer
+ * last modified date:23/03/2023
+ */
+#include<stdio.h>
+int main()
+{
+	printf("GREETING FORM RUGGEDBOARD TEAM\n");
+	return 0;
+}
+
